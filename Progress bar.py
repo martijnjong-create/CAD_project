ee84d55c45ff689cbbae5739466cafcbce3edd5b
martijnjong-create <61456221+martijnjong-create@users.noreply.github.com>
2021-04-19 12:12:22 +0200

    Add files via upload

---
+++ Progress bar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 16:49:52 2020
+
+@author: macbookpro
+"""
+
+from PIL import Image
+import pandas as pd
+
+# Read excelfile into dataframe
+df = pd.read_excel('excelfile')
+
+# Create green progress bar (length = 10 pixels per frame)
+im= Image.new("RGB", (len(df.Pred)*72, 200), "green")
+
+# Iterate over every row 
+for index, row in df.iterrows():
+    
+    # If prediction is not informative, make corresponding pixels red
+    if row["Pred"] != 0:
+        num = ((index*72), 0, (index*72+72), 200)
+        im.paste(("red"), num)
+
+im.show()
+im.save()
